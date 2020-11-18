2b76e5c582500983fb094449b387bde1b2bb27a9
phil <phil.rice@validoc.org>
2020-11-18 21:51:18 +0100

    Can now reload with different renderers

--- src/created/shas.js
+++ src/created/shas.js
@@ -1,5 +1,5 @@
 export let shas={//This is a bodge to avoid needing a server while we are still in the playground
-    Game: "created/Game/607f23157892a67cacb7530d9e62c34807fada57e59fbc578cba610b6c0bf450",
+    Game: "created/Game/49618584b7d9e2254117ef974829338b20e939a4ce7a955df33dfe0feb10c9ec",
     Board: "created/Board/a1fc5bbb21f36e1697d9e9e76ca8688b273f8e1154d7a02343c1c54af87ebe4b",
     Square: "created/Square/cf26cc50dc5ddc393cd85abb265e2ae1d2e7422cbd09799d59e2fcf00045f52b",
     Square2: "created/Square2/d559b7563d6f7009cbd96fb1c57de0a9349a4b1cde3fcf3076860002d6428d99"
--- src/domain/Game.tsx
+++ src/domain/Game.tsx
@@ -6,10 +6,24 @@ type R<T> = RestProperties<React.ReactElement, Domain, GameData, T>
 function Game(rest: R<GameData>): (props: any) => React.ReactElement {
     return props => {
         let newRest: R<BoardData> = rest.withLens(rest.domain().gameToBoardL);
+        function load(name: string) {
+            // @ts-ignore
+            let url = rest.json()._links[name].href
+            console.log(name, url)
+            rest.restRoot.reactRest.loadAndRender(url, rest.restRoot.setMainJson)
+        }
         return (<div className='game'>
             <div className='game-board'>
                 <Rest rest={newRest}/>
             </div>
+            <ul>
+                <li>
+                    <button onClick={() => load('game1')}>Square</button>
+                </li>
+                <li>
+                    <button onClick={() => load('game2')}>Square</button>
+                </li>
+            </ul>
         </div>)
     }
 }
