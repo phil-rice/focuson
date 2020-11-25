c9a8f79ed37dfa94d746c19e997bff1bc397b14c
Latika Sehgal <latika.mailbox@gmail.com>
2020-11-25 02:25:45 +0000

    UI changes

--- codeondemand/examples/cpq/src/render/SimpleFilter.tsx
+++ codeondemand/examples/cpq/src/render/SimpleFilter.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {CpqProperties, CqpFilter} from "../CpqDomain";
+import { CpqProperties, CqpFilter } from "../CpqDomain";
 
 
 
 function SimpleFilter<Main>(props: CpqProperties<Main, CqpFilter>) {
-    const onChange = (event: any) => { console.log("onChange.target", event.target.value)};
+    const onChange = (event: any) => { console.log("onChange.target", event.target.value) };
     let filterJson = props.context.json();
     let options = props.context.domain.makeOptions(filterJson.selected, props.context.json().legalValues);
-    return (<div key={filterJson.filterName} className={filterJson.filterName}><p>{filterJson.filterName}</p><select onChange={event => onChange(event)}>{options}</select></div>);
+    return (<div key={filterJson.filterName} className={filterJson.filterName}>
+        <p><span className="font-weight-bold">Filter:</span> {filterJson.filterName}</p>
+        <select className="custom-select custom-select-md mb-3" onChange={event => onChange(event)} key={`select-${filterJson.filterName}`}>{options}</select>
+    </div>);
 
 }
