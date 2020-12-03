097f942a51d80f20ca48efa9aceba1c69a34f043
Latika Sehgal <latika.mailbox@gmail.com>
2020-12-03 11:35:49 +0000

    Babel config fix and CSS Changes

--- examples/codeondemand/cpq/src/render/GearboxFilter.tsx
+++ examples/codeondemand/cpq/src/render/GearboxFilter.tsx
@@ -4,13 +4,13 @@ import { Lens } from "@phil-rice/lens";
 
 
 let filterToSelectedL = new Lens((c: CqpFilter) => c.selected, (c: CqpFilter, s: string | null) => ({ ...c, selected: s }));
-export function GearboxFilter<Main>(props: CpqProperties<Main, CqpFilter>) {
+function GearboxFilter<Main>(props: CpqProperties<Main, CqpFilter>) {
     let context = props.context;
     const onChange = (event: any) => context.setFrom(filterToSelectedL, event.target.value);
     let filterJson = context.json();
     let options = context.domain.makeOptions(filterJson.selected, context.json().legalValues);
-    return (<div key={filterJson.filterName}>
+    return (<div key={filterJson.filterName} className="gearboxFilterContainer">
         <p><span className="font-weight-bold">Filter:</span> {filterJson.filterName}</p>
-        <select className="custom-select custom-select-md mb-3" onChange={event => onChange(event)} key={`select-${filterJson.filterName}`}>{options}</select>
+        <select className="gearboxFilter" onChange={event => onChange(event)} key={`select-${filterJson.filterName}`}>{options}</select>
     </div>);
 }
--- examples/codeondemand/cpq/src/render/SimpleFilter.tsx
+++ examples/codeondemand/cpq/src/render/SimpleFilter.tsx
@@ -3,14 +3,14 @@ import { CpqProperties, CqpFilter } from "../CpqDomain";
 
 
 
-export function SimpleFilter<Main>(props: CpqProperties<Main, CqpFilter>) {
+function SimpleFilter<Main>(props: CpqProperties<Main, CqpFilter>) {
     const onChange = (event: any) => { console.log("onChange.target", event.target.value) };
     let filterJson = props.context.json();
-    let options =filterJson.legalValues.map(o => (<option key={o}>{o}</option>))
-    return ((<select className='simpleFilter'
-                     value={filterJson.selected ? filterJson.selected : ''}
-                     key={props.context.json().filterName}
-                     id={props.context.json().filterName}
-                     onChange={onChange}>{options}</select>));
+    let options = filterJson.legalValues.map(o => (<option key={o}>{o}</option>))
+    return (<div className="simpleFilterContainer"><select className='simpleFilter'
+        value={filterJson.selected ? filterJson.selected : ''}
+        key={props.context.json().filterName}
+        id={props.context.json().filterName}
+        onChange={onChange}>{options}</select></div>);
 
 }
