3e00921a7c1a6c3d914de332418f242b30c85a19
phil <phil.rice@validoc.org>
2021-02-20 12:41:26 +0100

    Adding prepare and ideify as scripts to allow monorepo usage

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ This project is a number of projects bundled together in a mono repo
 If you are interested in
 * [State management for React using Lens](modules/lens/README.md)
 * [Code of Demand for Restful servers that are resiliant to change](modules/codeondemand/README.md)
+
+# Working with the project
+
+You can optionally use [the laoban tool](https://www.npmjs.com/package/laoban) to help manage the monorepo.
+
+If you do so you need to 'install it'. If you are in the root directory then you
+can ask `npm update -g laoban`
---
+++ scripts/prepare.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+me=prepare.sh
+if [ $# -ne  0 ]; then
+   echo "usage $me
+
+   Searchs for all .ts and .tsx files in the current directory and below.
+   For each one if there was an import of 'from' that had been changed by ideify.sh, and reverses the change
+   It does this by looking for '//changed from [...]'
+
+   examples
+       $me @phil-rice/lens ../../modules/lens
+
+"
+ exit 2
+ fi
+
+grep -R --exclude-dir=node_modules --exclude-dir=dist "import.*; //changed from"  --include='*.tsx' --include='*.ts' -l  | xargs -L1 -r  sed "s#\"\(.*\)\; //changed from \(.*\);#\"\2\";#" -i
